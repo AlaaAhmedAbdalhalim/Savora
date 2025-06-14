bf98101d8868c06298019a38e3553d957d118286
unknown <alaaahmedabdalhalim@gmail.com>
2025-06-14 22:20:04 +0300

    Our Story Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { StoryComponent } from "./components/story/story.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, StoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
